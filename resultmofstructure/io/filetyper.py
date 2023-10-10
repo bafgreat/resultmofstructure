40a0fd1570c4c84bf334e69a5ea5dcc11dc842b9
bafgreat <75862753+bafgreat@users.noreply.github.com>
2023-10-11 00:42:08 +0200

    debug

--- resultmofstructure/io/filetyper.py
+++ resultmofstructure/io/filetyper.py
@@ -133,6 +133,7 @@ def ams_energetic(output):
     for line in parameters:
         if "Total Energy (hartree)" in line:
             total = float(line.split()[3])
+    for line in parameters:
         if "Energy (hartree)" in line:
             total = float(line.split()[2])
     return total
